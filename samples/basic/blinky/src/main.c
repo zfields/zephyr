620b88f59ddb0db0b053e49e2d54608d48095b69
Zachary J. Fields <zachary_fields@yahoo.com>
2023-02-10 11:51:45 -0600

    diff: Minimal changes to send Note

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -7,8 +7,17 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
+// Include Notecard note-c library
+#include "note.h"
+
+// Notecard node-c helper methods
+#include "notecard.h"
+
+// Set your ProductUID Here
+#define PRODUCT_UID "com.zakoverflow.test"
+
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#define SLEEP_TIME_MS   10000
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
@@ -23,6 +32,24 @@ void main(void)
 {
 	int ret;
 
+    // Initialize note-c references
+    NoteSetFnDefault(malloc, free, platform_delay, platform_millis);
+    NoteSetFnI2C(NOTE_I2C_ADDR_DEFAULT, NOTE_I2C_MAX_DEFAULT, noteI2cReset,
+                 noteI2cTransmit, noteI2cReceive);
+
+    // Send a Notecard hub.set using note-c
+    {
+        J *req = NoteNewRequest("hub.set");
+        if (req) {
+            JAddStringToObject(req, "product", PRODUCT_UID);
+            JAddStringToObject(req, "mode", "continuous");
+            JAddStringToObject(req, "sn", "zephyr-blink");
+            if (!NoteRequest(req)) {
+                return;
+            }
+        } else { return; }
+    }
+
 	if (!gpio_is_ready_dt(&led)) {
 		return;
 	}
@@ -33,6 +60,17 @@ void main(void)
 	}
 
 	while (1) {
+        {
+            J *req = NoteNewRequest("hub.log");
+            if (req) {
+                JAddStringToObject(req, "text", "Hello, World!");
+                JAddBoolToObject(req, "sync", true);
+                if (!NoteRequest(req)) {
+                    return;
+                }
+            } else { return; }
+        }
+
 		ret = gpio_pin_toggle_dt(&led);
 		if (ret < 0) {
 			return;
